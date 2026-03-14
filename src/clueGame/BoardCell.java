11150691de5fffa72bd704f0b63ae30964e080a7
Judith Rodriguez <judith_rodriguezchacon@mines.edu>
2026-03-13 18:14:01 -0600

    BoardCell has all the methods now

--- src/clueGame/BoardCell.java
+++ src/clueGame/BoardCell.java
@@ -2,17 +2,44 @@ package clueGame;
 
 import java.util.*;
 
+import experiment.TestBoardCell;
+
 public class BoardCell {
 	private int row;
 	private int col;
 	char initial;
+	
 	DoorDirection doorDirection;
 	private boolean roomLabel;
 	private boolean roomCenter;
 	private char secretPassage;
+	
+	
 	private Set<BoardCell> adjList;
 	
 	public void addAdj(BoardCell adj) {
-		
+		adjList.add(adj);
 	}
+	
+	public Set<BoardCell> getAdjList(){
+		return adjList; 
+	}
+	
+	private boolean roomLabel() {
+		return roomLabel;
+	}
+	
+	private boolean roomCenter() {
+		return roomCenter;
+	}
+	
+	private char secretPassage() {
+		return secretPassage;
+	}
+	
+	
+	
+	
+	
 }
+
--- src/experiment/TestBoardCell.java
+++ src/experiment/TestBoardCell.java
@@ -1,5 +1,4 @@
 package experiment;
-
 import java.util.*;
 
 
