450a42a396edf4f24cc8184b3739f5d400167320
Judith Rodriguez <judith_rodriguezchacon@mines.edu>
2026-03-13 23:24:40 -0600

    Created board and took in file input

--- src/clueGame/Board.java
+++ src/clueGame/Board.java
@@ -30,28 +30,102 @@ public class Board {
     /*
      * initialize the board (since we are using singleton pattern)
      */
-	public void initialize() {
-		try {
-			loadSetupConfig();
-			loadLayoutConfig();
-		}
-		catch(BadConfigFormatException e){
-			System.out.println(e);
-		}
+    //THIS MIGHT NOT BE THE BEST IDEA BUT IT'S WORKING SO FAR -- MIGHT NEED TO CHANGE FOR A TRY CATCH
+	public void initialize() throws BadConfigFormatException, FileNotFoundException {
+		loadLayoutConfig();
 	}
-<<<<<<< HEAD
-	
-	public void loadSetupConfig() throws FileNotFoundException {
-		Scanner scanner = new Scanner(new File("setupConfigFiles"));
-=======
-
-	public void loadSetupConfig() {
+	public void loadSetupConfig() throws FileNotFoundException{
+		roomMap = new HashMap<>();
 		
->>>>>>> faa7566719ba661a3360c831a291499322e31641
+		Scanner scanner = new Scanner(new File(setupConfigFiles));
+		
+		while (scanner.hasNextLine()){
+			String line = scanner.nextLine().trim();
+			
+			String[] parts = line.split(",");
+			
+			//whenever we dont have three eventually an exception
+			
+			String type = parts[0].trim();
+			String name = parts[1].trim();
+			String symbol = parts[2].trim();
+			
+			char initial = symbol.charAt(0);
+			roomMap.put(initial, new Room (name, null, null));
+		}
+		scanner.close();
+		
 	}
-	
-	public void loadLayoutConfig() {
-		Scanner scanner = new Scanner(new File("layoutConfigFiles"));
+	public void loadLayoutConfig() throws FileNotFoundException {
+		ArrayList<String[]> fileLines = new ArrayList<>();
+		Scanner scanner = new Scanner(new File(layoutConfigFiles));
+		
+		int expectedColumns = -1; 
+		while (scanner.hasNextLine()) {
+			String line = scanner.nextLine().trim();
+			String[] parts = line.split(",");
+			
+			if (expectedColumns == -1) {
+				expectedColumns = parts.length;
+			} else  if (parts.length != expectedColumns){
+				scanner.close();
+				//WE MIGHT WANT TO THROW AN EXCPETION HERE 
+			}
+			fileLines.add(parts);
+			
+		}
+		scanner.close();
+		
+		numRows = fileLines.size();
+		numColumns = expectedColumns;
+		
+		grid = new BoardCell[numRows][numColumns];
+		for (int row = 0; row < numRows; row++) {
+			String[] currentRow = fileLines.get(row);
+			
+			for (int col = 0; col < numColumns; col++) {
+				String cellCode = currentRow[col].trim();
+				
+				//each cell must be length  1 or 2 n
+				if (cellCode.length() < 1 || cellCode.length() > 2) {
+					//Throw exception
+				}
+				char initial = cellCode.charAt(0);
+				BoardCell cell = new BoardCell(row,col,initial);
+				
+				//special cases
+				if (cellCode.length() == 2) {
+					char marker = cellCode.charAt(1);
+					
+					if (marker == '<') {
+						cell.setDoorDirection(DoorDirection.LEFT);
+					}else if (marker == '>') {
+						cell.setDoorDirection(DoorDirection.RIGHT);
+					}else if (marker == '^') {
+						cell.setDoorDirection(DoorDirection.UP);
+					}else if (marker == 'v') {
+						cell.setDoorDirection(DoorDirection.DOWN);
+					}else if (marker == '*') {
+						cell.setLabel(true);
+						roomMap.get(initial).setLabelCell(cell);
+					}else if (marker == '#') {
+						cell.setLabel(true);
+						roomMap.get(initial).setLabelCell(cell);
+					}else if (marker == '>') {
+						cell.setDoorDirection(DoorDirection.RIGHT);
+					}else if (marker == 'D') {
+						cell.setSecretPassage('G');
+					}else if (marker == 'G') {
+						cell.setSecretPassage('D');
+					}else if (marker == 'P') {
+						cell.setSecretPassage('M');
+					}else if (marker == 'M') {
+						cell.setSecretPassage('P');
+					}
+				}
+				grid[row][col] = cell;
+			}
+		}
 	}
 
 	public void setConfigFiles(String setupConfigFiles, String layoutConfigFiles) {
