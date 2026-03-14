d9d527561fc51bb5f451679ddebdfc6ea415c3f9
JadeAndAndi <eharlsma17@gmail.com>
2026-03-13 21:28:07 -0600

    added txt explinations

--- src/clueGame/BadConfigFormatException.java
+++ src/clueGame/BadConfigFormatException.java
@@ -2,6 +2,10 @@ package clueGame;
 
 //special exception message for bad data input
 public class BadConfigFormatException extends Exception {
+	public BadConfigFormatException() {
+		super("The File input was configured poorly.");
+	}
+	
 	public BadConfigFormatException(String message) {
 		super(message);
 	}
